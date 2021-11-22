d4f0e63a313c7319018169f8826200524f3c27c5
SmartGlaxx <52786384+SmartGlaxx@users.noreply.github.com>
2021-11-22 13:22:15 +0100

    Add layer of Auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,24 +6,29 @@ const registerUser = async(req, res, next)=>{
     const username = req.body.username.toLowerCase()
     const email = req.body.email.toLowerCase()
     const password = req.body.password
-   try{ 
-        if(password.length < 8){
-            return res.status(200).json({response: "Fail", message : "Password cannot be less than 8 characters"})
-        }else{
-            const salt = await bcrypt.genSalt(10)
-            const hashedPasword = await bcrypt.hash(password, salt)
-            const checkUser = await User.find().or([{email : email}, {username : username}])
 
-            if(checkUser.length > 0){
-                return res.status(200).json({response: "Fail", message : "Username or email is registred. Please sign-in"})
+    if(!username || !email || !password){
+       try{ 
+            if(password.length < 8){
+                return res.status(200).json({response: "Fail", message : "Password cannot be less than 8 characters"})
             }else{
-                const singupdData = await User.create({ username, email , password : hashedPasword })
-                return res.status(200).json({response : "Success", data : singupdData})
-            }
+                const salt = await bcrypt.genSalt(10)
+                const hashedPasword = await bcrypt.hash(password, salt)
+                const checkUser = await User.find().or([{email : email}, {username : username}])
 
+                if(checkUser.length > 0){
+                    return res.status(200).json({response: "Fail", message : "Username or email is registred. Please sign-in"})
+                }else{
+                    const singupdData = await User.create({ username, email , password : hashedPasword })
+                    return res.status(200).json({response : "Success", singupdData})
+                }
+
+            }
+        }catch(error){
+            res.status(200).json({response : "Fail", message : "An error occured. Please try again"})
         }
-    }catch(error){
-        res.status(200).json({response : "Fail", message : "An error occured. Please try again"})
+     }else{
+         res.status(200).json({response: "Fail", message : "Please enter your Username, E-mail and Password"})
     }
 }
 
@@ -34,24 +39,28 @@ const loginUser = async(req, res)=>{
     const emailOrUsername = req.body.emailOrUsername.toLowerCase()
     const password = req.body.password
 
-    try{ 
-        const loginData = await User.findOne({$or : [{email : emailOrUsername}, {username : emailOrUsername}]})
-        const checkedEmail = loginData.email 
-        const checkedUsername = loginData.username 
-        const storedPassword = loginData.password 
-        
-    if((checkedEmail === emailOrUsername) || (checkedUsername === emailOrUsername)){
-        const checkedPassword = await bcrypt.compare(password, storedPassword)
-        if(!checkedPassword){
-            return res.status(200).json({response : "Fail", message : "Password Incorrect"})
+    if(!emailOrUsername  || !password){
+        try{ 
+            const loginData = await User.findOne({$or : [{email : emailOrUsername}, {username : emailOrUsername}]})
+            const checkedEmail = loginData.email 
+            const checkedUsername = loginData.username 
+            const storedPassword = loginData.password 
+            
+        if((checkedEmail === emailOrUsername) || (checkedUsername === emailOrUsername)){
+            const checkedPassword = await bcrypt.compare(password, storedPassword)
+            if(!checkedPassword){
+                return res.status(200).json({response : "Fail", message : "Password Incorrect"})
+            }
+            return res.status(200).json({response : "Success", loginData})
+        }else{
+            return res.status(200).json({response: "Fail", message : "Email or Username not found in our database. Please try again"})
         }
-        return res.status(200).json({response : "Success", data : loginData})
+        
+        }catch(error){
+            res.status(200).json({response: "Fail", message : "User or Username not found. Please try again"})
+        }
     }else{
-        return res.status(200).json({response: "Fail", message : "Email or Username not found in our database. Please try again"})
-    }
-    
-    }catch(error){
-        res.status(200).json({response: "Fail", message : "User or Username not found. Please try again"})
+         res.status(200).json({response: "Fail", message : "Please enter your E-mail / Username and Password"})
     }
 }
 
